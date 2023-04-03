334577462bd7cb18ca59b9155ccaa0c0fafacedf
Александр <alexandr-d23@mail.ru>
2023-04-03 17:29:05 +0300

    completed task3

--- src/main/kotlin/com/example/inf_search_web/task3/BooleanSearch.kt
+++ src/main/kotlin/com/example/inf_search_web/task3/BooleanSearch.kt
@@ -2,6 +2,8 @@ package task3
 
 import java.io.File
 import java.io.FileOutputStream
+import java.io.FileReader
+import java.util.*
 
 class BooleanSearch {
 
@@ -34,4 +36,122 @@ class BooleanSearch {
         }
         return lemmasWithIndex
     }
+
+    fun searchByOperations() {
+        val words = mutableMapOf<String, List<String>>().also { map ->
+            FileReader(File("src/main/kotlin/result/task3/inverted_list")).readLines()
+                .forEach {
+                    val s = it.split(":")
+                    val key = s[0]
+                    val list = s[1].split(" ").filter { st -> st.isNotEmpty() }
+                    map.put(key, list)
+                }
+        }
+        println("Булевы операции: \"&\", \"|\", \"-\"! Все должно быть только через пробел!")
+
+        val sc = Scanner(System.`in`)
+        val string = sc.nextLine()
+        println(preparing(string, words))
+    }
+
+    fun preparing(condition: String, words: MutableMap<String, List<String>>): MutableList<List<String>> {
+
+        var wordsSet = mutableListOf<List<String>>()
+        var userSearch: List<String> = condition.split(" ").map {
+            if (it == "&" || it == "|" || it == "-") {
+                it
+            } else {
+                Utils.getLemmasFromTokens(listOf(it)).firstNotNullOf {
+                    it.key
+                }
+            }
+        }
+
+        userSearch.forEach {
+            if (words.contains(it)) {
+                wordsSet.add(words.get(it)!!)
+            } else if (it == "&" || it == "|" || it == "-") {
+                wordsSet.add(listOf(it))
+            } else {
+                wordsSet.add(listOf())
+            }
+        }
+
+        var a1: List<String>
+        var a2: List<String>
+        var i = 1
+
+
+        while (i <= wordsSet.size - 2) {
+
+            if (wordsSet.size >= 3) {
+
+                a1 = wordsSet[i - 1]
+                a2 = wordsSet[i + 1]
+
+                if (wordsSet[i].contains("-")) {
+                    wordsSet[i] = not(a1, a2)
+                    wordsSet.removeAt(i + 1)
+                    wordsSet.removeAt(i - 1)
+                    i--
+                } else if (wordsSet[i].contains("&")) {
+                    wordsSet[i] = and(a1, a2)
+                    wordsSet.removeAt(i + 1)
+                    wordsSet.removeAt(i - 1)
+                    i--
+                } else if (wordsSet[i].contains("|")) {
+                    wordsSet[i] = or(a1, a2)
+                    wordsSet.removeAt(i + 1)
+                    wordsSet.removeAt(i - 1)
+                    i--
+                }
+            }
+            i++
+        }
+
+        return wordsSet
+    }
+
+
+    fun and(a1: List<String>, a2: List<String>): List<String> {
+
+        var list = mutableListOf<String>()
+
+        a1.forEach {
+            if (a2.contains(it))
+                list.add(it)
+        }
+        return list
+    }
+
+    fun or(a1: List<String>, a2: List<String>): List<String> {
+
+        var list = mutableListOf<String>()
+
+        a1.forEach {
+            if (!list.contains(it)) {
+                list.add(it)
+            }
+        }
+        a2.forEach {
+            if (!list.contains(it)) {
+                list.add(it)
+            }
+        }
+        return list
+    }
+
+    fun not(a1: List<String>, a2: List<String>): List<String> {
+
+        var list = mutableListOf<String>()
+
+        a1.forEach {
+            if (!a2.contains(it)) {
+                list.add(it)
+            }
+        }
+        return list
+    }
+
+
 }
